7b59a8e19c5a7ca237257e974b0a413d651486e3
Aaron <a.rcottrill521@gmail.com>
2018-05-15 17:32:29 -0400

    Add some functionality to resemble the "dir" command.

--- CoreDirectoryApp/Program.cs
+++ CoreDirectoryApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CoreDirectoryApp
 {
@@ -6,7 +7,32 @@ namespace CoreDirectoryApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String dirName = Directory.GetCurrentDirectory();
+            long size = 0; 
+            Console.Out.WriteLine("Directory of {0} \n", dirName); 
+            foreach (string filename in Directory.EnumerateFiles(dirName))
+            {
+                string realPath = Path.Combine(dirName, filename);
+                //FileAttributes attributes = File.GetAttributes(realPath); 
+                
+                //DateTime FileCreationDate = File.GetCreationTime(realPath);
+                FileInfo fileInfo = new FileInfo(realPath);
+                size += fileInfo.Length; 
+                if(fileInfo.Attributes == FileAttributes.Directory)
+                {
+                    Console.Out.WriteLine(" Created: {0} , <Dir> , Name: {1}", fileInfo.CreationTime.ToShortDateString(), fileInfo.Name);
+                }
+                else
+                {
+                    Console.Out.WriteLine(" Created: {0} , Size: {1} bytes, Name: {2} ", fileInfo.CreationTime.ToShortDateString(), fileInfo.Length, fileInfo.Name);
+                }
+              
+                Console.Out.WriteLine("Total Size: {0} bytes",size);
+
+                
+            }
+            Console.ReadKey(); 
+                
         }
     }
 }
