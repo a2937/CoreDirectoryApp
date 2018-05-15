65c38fcc4791c725e83fba3469a837712aac40cf
Aaron <a.rcottrill521@gmail.com>
2018-05-15 13:50:25 -0400

    Add project files.

---
+++ CoreDirectoryApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreDirectoryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
